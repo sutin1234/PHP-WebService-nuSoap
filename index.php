c619513639f344a1bcf595c5faad648f42aed487
sutin <tony.sutin1234@gmail.com>
2015-09-24 23:21:08 +0700

    WebService nuSoap PHP
     Created WebService  By nuSoap Libraly

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+	require_once("nusoap-0.9.5/lib/nusoap.php");
+	function getValue($rpcNumber,$inStr){
+		
+		$client = new nusoap_client("http://27.131.163.203/testws/service.asmx?wsdl",true);
+		$params = array(
+					"rpcNumber" => $rpcNumber,
+					"inStr" => $inStr
+				);
+		$data = $client->call("CallTHI",$params);
+		print_r($data);
+		echo "<hr />";
+	}
+	
+	if(!empty($_POST)){
+		getValue($_POST['rpcNumber'],$_POST['inStr']);
+	}
+?>
+<form method="post">
+	rpcNumber : <input type="text" required name="rpcNumber"> <br />
+	inStr : <input type="text" required name="inStr"> <br />
+	<input type="submit" value="submit"/>
+</form>
